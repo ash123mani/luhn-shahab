6a0ff955b48f2d199e0c5386d9cd4a0b6ffc3e6f
Ashutosh <ashuma2721@gmail.com>
2023-08-13 10:38:10 +0530

    updated functions

--- packages/server/netlify/functions/api.ts
+++ packages/server/netlify/functions/api.ts
@@ -4,7 +4,8 @@ import { healthCheckRouter } from '@/routes/healthcheck';
 import { validateRouter } from '@/routes/validate';
 import path from 'path';
 
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use('/', (req, res) => res.send("Hola"));
 
 app.use('/.netlify/functions/api/healthcheck', healthCheckRouter);  // path must route to lambda
 app.use('/.netlify/functions/api/validate', validateRouter);  // path must route to lambda
