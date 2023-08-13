f68fe98f320bbfdadbd167b62b5fc2a922d1f0f9
Ashutosh <ashuma2721@gmail.com>
2023-08-13 10:18:47 +0530

    updated routes

--- packages/server/netlify/functions/api.ts
+++ packages/server/netlify/functions/api.ts
@@ -1,4 +1,7 @@
 import serverless from 'serverless-http';
 import { app } from '@/index';
+import path from 'path';
+
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 export const handler = serverless(app);
