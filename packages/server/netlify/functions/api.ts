36273d195d0575cc5198b70355185e362cb6ae7c
Ashutosh <ashuma2721@gmail.com>
2023-08-13 10:30:58 +0530

    updated build command

--- packages/server/netlify/functions/api.ts
+++ packages/server/netlify/functions/api.ts
@@ -1,7 +1,12 @@
 import serverless from 'serverless-http';
 import { app } from '@/index';
+import { healthCheckRouter } from '@/routes/healthcheck';
+import { validateRouter } from '@/routes/validate';
 import path from 'path';
 
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use('/.netlify/functions/api/healthcheck', healthCheckRouter);  // path must route to lambda
+app.use('/.netlify/functions/api/validate', validateRouter);  // path must route to lambda
 
 export const handler = serverless(app);
