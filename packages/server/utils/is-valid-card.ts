3c4cd6e73539664aecee625f603a2becb4eaeccc
ash123mani <36326714+ash123mani@users.noreply.github.com>
2023-09-27 13:04:21 +0530

    Update is-valid-card.ts

--- packages/server/utils/is-valid-card.ts
+++ packages/server/utils/is-valid-card.ts
@@ -1,31 +1,14 @@
 export const isValidCard = (cardNum: string): boolean => {
-
-
 	if (/[^0-9-\s]+/.test(cardNum)) return false;
-
-
+	
 	let arr = (cardNum + '')
 		.split('')
 		.reverse()
 		.map(x => parseInt(x));
+	
 	let lastDigit = arr.splice(0, 1)[ 0 ];
 	let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
 	sum += lastDigit;
+	
 	return sum % 10 === 0;
-
-	// The Luhn Algorithm. It's so pretty.
-	// let nCheck = 0, bEven = false;
-	// cardNum = cardNum.replace(/\D/g, "");
-
-	// for (var n = cardNum.length - 1; n >= 0; n--) {
-	// 	var cDigit = cardNum.charAt(n),
-	// 		  nDigit = parseInt(cDigit, 10);
-
-	// 	if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
-
-	// 	nCheck += nDigit;
-	// 	bEven = !bEven;
-	// }
-
-	// return (nCheck % 10) == 0;
 }
